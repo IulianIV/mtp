0b8f96a1d2902851f98b1d1e1543a458ce172ebc
Iulian Ichim <ichim.iulian12@gmail.com>
2021-05-12 00:04:05 +0300

    updated README and schema

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mtp
+MTP is a web app that contains several trackers.
+1. Budgeting;
+2. Health Tracking;
+4. Media Collections;
+3. Small blog and testing;
+
+Each app will have a `mtp/<app-name>` path followed by several views. Example:
+`mtp/budget/summary/html`
--- mtp/schema.sql
+++ mtp/schema.sql
@@ -1,5 +1,13 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS budget_expense;
+DROP TABLE IF EXISTS budget_revenue;
+DROP TABLE IF EXISTS budget_savings;
+DROP TABLE IF EXISTS budget_utilities;
+DROP TABLE IF EXISTS collections_ledger;
+DROP TABLE IF EXISTS collections_media;
+DROP TABLE IF EXISTS health_biometrics;
+DROP TABLE IF EXISTS health_workout;
 
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,3 +23,15 @@ CREATE TABLE post (
     body TEXT NOT NULL,
     FOREIGN KEY (author_id) REFERENCES user (id)
 );
+
+
+
+validation_tables = ['types', 'levels', 'regions', 'workouts', 'reps', 'sets']
+
+types = ['body weight', 'weighted']
+levels = ['beginner', 'intermediate', 'advanced']
+regions = ['Anterior Lower - Squats', 'Upper Push', 'Posterior Lower - Hinge', 'Upper Pull', 'Abs', 'Corrective',
+           'Anterior Lower - Lunge', 'Calf', 'Forearm', 'Postural', 'Stretch']
+workouts = ['Workout A', 'Workout B', 'Variation A', 'Variation B', 'Variation C', 'Rest day']
+reps = ['8-12']
+sets = ['3', '1 minute', '45 seconds', 'failure']
