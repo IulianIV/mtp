09d670e3d3ee73e943ab3bfce2b485822252245e
Iulian Ichim <ichim.iulian12@gmail.com>
2021-05-21 00:34:21 +0300

    Heavily modified summary view to include multiple queries through standard SELECT or special class object SELECT.

--- mtp/budget.py
+++ mtp/budget.py
@@ -1,10 +1,11 @@
+import werkzeug.wsgi
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
 
 from mtp.auth import login_required
-from mtp.db import get_db
+from mtp.db import get_db, close_db
 
 bp = Blueprint('budget', __name__, url_prefix='/budget')
 
@@ -15,19 +16,58 @@ bp = Blueprint('budget', __name__, url_prefix='/budget')
 #     return render_template('budget/index.html')
 #     pass
 
-
 @bp.route('/')
 @login_required
 def summary():
     db = get_db()
-    entries = db.execute(
+
+    expense_entries = db.execute(
         'SELECT id, expense_date, expense_item, expense_value, expense_item_category, expense_source'
-        ' FROM budget_expense'
-        ' ORDER BY expense_date ASC'
+        ' FROM budget_expense '
+        'ORDER BY expense_date ASC'
     ).fetchall()
 
-    return render_template('budget/summary.html', entries=entries)
+    revenue_entries = db.execute(
+        'SELECT id, revenue_date, revenue_value, revenue_source'
+        ' FROM budget_revenue '
+        'ORDER BY revenue_date ASC'
+    ).fetchall()
 
+    savings_entries = db.execute(
+        'SELECT id, savings_date,savings_value, savings_source, savings_reason, savings_action'
+        ' FROM budget_savings '
+        'ORDER BY savings_date ASC'
+    ).fetchall()
+
+    # Class object for handling multiple SQL queries, might be useful in cases where above code does not work
+
+    # class BudgetDbConnector:
+    #     def __init__(self):
+    #         self.db = get_db()
+    #
+    #     @property
+    #     def expense_entries(self):
+    #         return self.db.execute(
+    #             'SELECT id, expense_date, expense_item, expense_value, expense_item_category, expense_source'
+    #             ' FROM budget_expense '
+    #             'ORDER BY expense_date ASC')
+    #
+    #     @property
+    #     def revenue_entries(self):
+    #         return self.db.execute(
+    #             'SELECT id, revenue_date, revenue_value, revenue_source'
+    #             ' FROM budget_revenue '
+    #             'ORDER BY revenue_date ASC')
+    #
+    #     @property
+    #     def savings_entries(self):
+    #         return self.db.execute(
+    #             'SELECT id, savings_date,savings_value, savings_source, savings_reason, savings_action'
+    #             ' FROM budget_savings '
+    #             'ORDER BY savings_date ASC')
+
+    return render_template('budget/summary.html', expense_entries=expense_entries, revenue_entries=revenue_entries,
+                           savings_entries=savings_entries)
 
 @bp.route('/new-expense-entry', methods=('GET', 'POST'))
 @login_required
@@ -58,7 +98,7 @@ def add_expense_entry():
                 'VALUES (?, ?, ?, ?, ?)', (date, item, value, item_category, source)
             )
             db.commit()
-            return redirect(url_for('budget.summary'))
+            return redirect(url_for('budget.add_expense_entry'))
 
         flash(error)
     return render_template('budget/expense.html')
@@ -86,7 +126,7 @@ def add_revenue_entry():
                 'VALUES (?, ?, ?)', (date, revenue, source)
             )
             db.commit()
-            return redirect(url_for('budget.summary'))
+            return redirect(url_for('budget.add_revenue_entry'))
 
         flash(error)
 
@@ -108,7 +148,7 @@ def add_savings_entry():
         if not date:
             error = 'Date is required'
         elif not value:
-            error = 'Value is required'
+            error = '"Value" is required'
         elif not source:
             error = 'Source is required'
         elif not reason:
@@ -117,13 +157,41 @@ def add_savings_entry():
             error = 'Action is required'
         elif error is None:
             db.execute(
-                'INSERT INTO budget_savings (savings_date, savings_value, savings_source'
-                'savings_reasons, savings_action) VALUES (?, ?, ?, ?, ?)',
+                'INSERT INTO budget_savings (savings_date, savings_value, savings_source,'
+                'savings_reason, savings_action) VALUES (?, ?, ?, ?, ?)',
                 (date, value, source, reason, action)
             )
             db.commit()
-            return redirect(url_for('budget.summary'))
+            return redirect(url_for('budget.add_savings_entry'))
 
         flash(error)
 
     return render_template('budget/savings.html')
+
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update_entry():
+    return render_template('budget/update.html')
+    pass
+
+
+@login_required
+@bp.route('/statistics')
+def statistics():
+    return render_template('budget/statistics.html')
+    pass
+
+
+@bp.route('/report')
+@login_required
+def report():
+    return render_template('budget/report.html')
+    pass
+
+
+@bp.route('/lookup')
+@login_required
+def lookup():
+    return render_template('budget/lookup.html')
+    pass
