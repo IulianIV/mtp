8e70af7a02f04acef34b032abeb6314ed2d45e6f
Iulian Ichim <ichim.iulian12@gmail.com>
2021-08-28 12:59:50 +0300

    some code aanitization

--- mtp/db.py
+++ mtp/db.py
@@ -17,7 +17,7 @@ def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
-        db.close() #
+        db.close()
 
 
 def init_db():
@@ -32,7 +32,7 @@ def init_db():
 def init_db_command():
     """Clear the existing data and create new tables"""
     init_db()
-    click.echo('Initialized the databse.')
+    click.echo('Initialized the database.')
 
 
 def init_app(app):
