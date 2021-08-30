9e1d19b10847e91dfc73c878052c96b714abd94a
Iulian Ichim <ichim.iulian12@gmail.com>
2021-08-30 22:30:52 +0300

    Minor code cleanup.

--- mtp/mtp.py
+++ mtp/mtp.py
@@ -1,13 +1,9 @@
-"""
-Main Blog template
-
-
-"""
-
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
+from flask_wtf import FlaskForm
+from wtforms.fields import TextField, TextAreaField, SubmitField
 
 from mtp.auth import login_required
 from mtp.db import get_db
@@ -15,99 +11,141 @@ from mtp.db import get_db
 bp = Blueprint('mtp', __name__)
 
 
+class BlogDbConnector:
+    def __init__(self):
+        self.db = get_db()
+
+    @property
+    def query_blog_posts(self):
+        return self.db.execute(
+            'SELECT p.id, title, body, created, author_id, username'
+            ' FROM post p JOIN user u ON p.author_id = u.id'
+            ' ORDER BY created DESC'
+        ).fetchall()
+
+    def query_blog_post(self, post_id):
+        return self.db.execute(
+            'SELECT p.id, title, body, created, author_id, username'
+            ' FROM post p JOIN user u ON p.author_id = u.id'
+            ' WHERE p.id = ?',
+            (post_id,)
+        ).fetchone()
+
+    def insert_post(self, title, body, user_id):
+        return self.db.execute(
+            'INSERT INTO post (title, body, author_id)'
+            ' VALUES (?, ?, ?)',
+            (title, body, user_id)
+        )
+
+    def update_post(self, title, body, post_id):
+        return self.db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, post_id)
+            )
+
+    def delete_post(self, post_id):
+        return self.db.execute('DELETE FROM post WHERE id = ?', (post_id,))
+
+
+class AddPost(FlaskForm):
+    post_title = TextField()
+    post_body = TextAreaField()
+    submit_post = SubmitField()
+
+
+class UpdatePost(FlaskForm):
+    update_title = TextField()
+    update_body = TextAreaField()
+    submit_update = SubmitField()
+
+
 @bp.route('/')
 def index():
-    db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
+    blog_connect = BlogDbConnector()
+
+    posts = blog_connect.query_blog_posts
     return render_template('mtp/index.html', posts=posts)
 
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
+    db = get_db()
+    blog_connect = BlogDbConnector()
+    post_form = AddPost()
+
+    user_id = g.user['id']
+
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+
+        title = post_form.post_title.data
+        body = post_form.post_body.data
+
         error = None
 
         if not title:
             error = 'Title is required.'
-
         if error is not None:
             flash(error)
         else:
-            db = get_db()
-            db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
-            )
+            blog_connect.insert_post(title, body, user_id)
             db.commit()
             return redirect(url_for('mtp.index'))
 
-    return render_template('mtp/create.html')
+    return render_template('mtp/create.html', post_form=post_form)
 
 
-def get_post(id, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
+def get_post(post_id, check_author=True):
+    blog_connect = BlogDbConnector()
+    user_id = g.user['id']
+
+    post = blog_connect.query_blog_post(post_id)
 
     if post is None:
-        abort(404, "Post id {0} doesn't exist.".format(id))
-
-    if check_author and post['author_id'] != g.user['id']:
+        abort(404, "Post id {0} doesn't exist.".format(post_id))
+    if check_author and post['author_id'] != user_id:
         abort(403)
 
     return post
 
 
-"""
-Form catcher that adds entries to `post` database 
-
-"""
-
-
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@bp.route('/<int:post_id>/update', methods=('GET', 'POST'))
 @login_required
-def update(id):
-    post = get_post(id)
+def update(post_id):
+    db = get_db()
+    blog_connect = BlogDbConnector()
+
+    update_form = UpdatePost()
+
+    post = get_post(post_id)
+
+    # fixme find new method to show a preview of the previous
+    #   posts` body. <textarea> does not seem to have this.
 
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
 
-        if not title:
-            error = 'Title is required.'
+        title = update_form.update_title.data
+        body = update_form.update_body.data
 
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE post SET title = ?, body = ?'
-                ' WHERE id = ?',
-                (title, body, id)
-            )
-            db.commit()
-            return redirect(url_for('mtp.index'))
+        blog_connect.update_post(title, body, post['id'])
+        db.commit()
 
-    return render_template('mtp/update.html', post=post)
+        return redirect(url_for('mtp.index'))
+
+    return render_template('mtp/update.html', post=post, update_form=update_form)
 
 
-@bp.route('/<int:id>/delete', methods=('POST',))
+@bp.route('/<int:post_id>/delete', methods=('POST', 'GET'))
 @login_required
-def delete(id):
-    get_post(id)
+def delete(post_id):
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    blog_connect = BlogDbConnector()
+
+    get_post(post_id)
+
+    blog_connect.delete_post(post_id)
     db.commit()
+
     return redirect(url_for('mtp.index'))
