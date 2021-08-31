b2bb5873ff82be9efa1534bed1929ecf8ca91528
Iulian Ichim <ichim.iulian12@gmail.com>
2021-09-01 00:11:48 +0300

    Adapted to connect to db module.

--- mtp/auth.py
+++ mtp/auth.py
@@ -5,7 +5,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from mtp.db import get_db
+from mtp.db_manager.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
