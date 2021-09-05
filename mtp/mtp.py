b35d55598e13c7eef58e655a0470bd7331f50e5a
Iulian Ichim <ichim.iulian12@gmail.com>
2021-09-05 15:58:05 +0300

    Changed design to be able to see the pages in the dashboard. Adapted micro blogging admin view to support said pages.

--- mtp/mtp.py
+++ mtp/mtp.py
@@ -7,6 +7,7 @@ from wtforms.fields import TextField, TextAreaField, SubmitField
 
 from mtp.auth import login_required
 from mtp.db_manager.db import get_db
+from mtp.db_manager.db_interrogations import Query, Insert, Update, Delete
 
 bp = Blueprint('mtp', __name__)
 
@@ -14,39 +15,26 @@ bp = Blueprint('mtp', __name__)
 class BlogDbConnector:
     def __init__(self):
         self.db = get_db()
+        self.db_queries = Query()
+        self.db_insert = Insert()
+        self.db_update = Update()
+        self.db_delete = Delete()
 
     @property
     def query_blog_posts(self):
-        return self.db.execute(
-            'SELECT p.id, title, body, created, author_id, username'
-            ' FROM post p JOIN user u ON p.author_id = u.id'
-            ' ORDER BY created DESC'
-        ).fetchall()
+        return self.db_queries.query_blog_posts()
 
     def query_blog_post(self, post_id):
-        return self.db.execute(
-            'SELECT p.id, title, body, created, author_id, username'
-            ' FROM post p JOIN user u ON p.author_id = u.id'
-            ' WHERE p.id = ?',
-            (post_id,)
-        ).fetchone()
+        return self.db_queries.query_blog_post(post_id)
 
     def insert_post(self, title, body, user_id):
-        return self.db.execute(
-            'INSERT INTO post (title, body, author_id)'
-            ' VALUES (?, ?, ?)',
-            (title, body, user_id)
-        )
+        return self.db_insert.insert_post(title, body, user_id)
 
     def update_post(self, title, body, post_id):
-        return self.db.execute(
-                'UPDATE post SET title = ?, body = ?'
-                ' WHERE id = ?',
-                (title, body, post_id)
-            )
+        return self.db_update.update_post(title, body, post_id)
 
     def delete_post(self, post_id):
-        return self.db.execute('DELETE FROM post WHERE id = ?', (post_id,))
+        return self.db_delete.delete_post(post_id)
 
 
 class AddPost(FlaskForm):
@@ -149,3 +137,24 @@ def delete(post_id):
     db.commit()
 
     return redirect(url_for('mtp.index'))
+
+
+@bp.route('/error-page/401', methods=('GET',))
+@login_required
+def error_401():
+    pass
+    return render_template('401.html')
+
+
+@bp.route('/error-page/404', methods=('GET',))
+@login_required
+def error_404():
+    pass
+    return render_template('404.html')
+
+
+@bp.route('/error-page/500', methods=('GET',))
+@login_required
+def error_500():
+    pass
+    return render_template('500.html')
