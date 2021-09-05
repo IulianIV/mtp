115ce1531547c257c98d11de3aa93333620338d2
Iulian Ichim <ichim.iulian12@gmail.com>
2021-09-05 15:20:54 +0300

    Grammar corrections.

--- mtp/db_manager/schemas/budget_utilities.sql
+++ mtp/db_manager/schemas/budget_utilities.sql
@@ -3,7 +3,7 @@ CREATE TABLE budget_utilities (
     utilities_date TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
     utilities_rent_value INTEGER NOT NULL,
     utilities_energy_value INTEGER NOT NULL,
-    utilities_sattelite_value INTEGER NOT NULL,
+    utilities_satellite_value INTEGER NOT NULL,
     utilities_maintenance_value INTEGER NOT NULL,
     utilities_info TEXT
 );
