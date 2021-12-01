b5f60ab969f09ddc9d68316cf5a2eba111774214
Iulian Ichim <ichim.iulian12@gmail.com>
2021-12-01 13:07:07 +0200

    Completed some TODOs

--- app/blog/__init__.py
+++ app/blog/__init__.py
@@ -3,3 +3,7 @@ from flask import Blueprint
 bp = Blueprint('blog', __name__)
 
 from app.blog import routes
+
+# TODO revamp the Blog functionality.
+#   show the posts specific to a blog not in a tabular form.
+#   add more data tot he posts table
