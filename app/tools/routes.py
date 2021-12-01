95ac22c5036cb196b0ef2fc97db4ba331e20f8e6
Iulian Ichim <ichim.iulian12@gmail.com>
2021-12-01 16:02:40 +0200

    Organized some file.

--- app/tools/routes.py
+++ app/tools/routes.py
@@ -1,25 +1,3 @@
-# import werkzeug.wsgi
-# from flask import (
-#     Blueprint, flash, g, redirect, render_template, request, url_for
-# )
-# from werkzeug.exceptions import abort
-#
-# from mtp.auth import login_required
-# from mtp.db import get_db, close_db
-#
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, DateField, IntegerField
-# from wtforms.validators import DataRequired
-# from mtp.app.tools import bp
-#
-#
-#
-#
-# @bp.route('/')
-# @login_required
-# def summary():
-#     pass
-
 # TODO URL shortener
 # TODO Catalog Tools
 # TODO Google Sheets and Excel Integrations
