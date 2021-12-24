bf135c47fea425ebfa6900c1c31f42ebf3ac2bc6
IulianIV <ichim.iulian12@gmail.com>
2021-12-24 14:15:57 +0200

    form to be able to generate fakes from web interface.

---
+++ app/manager/tests/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from app.manager.protection import CheckForNumber
+from wtforms.fields import SubmitField, TextAreaField, SelectField, BooleanField
+from wtforms.validators import DataRequired
+
+
+class AddFakes(FlaskForm):
+    fake_choices = SelectField('Choose what data to fake', validators=[DataRequired()])
+    fake_number = TextAreaField('Choose number of fakes to generate [range] (e.g. 5-25)', validators=[DataRequired(), CheckForNumber()])
+    have_params = BooleanField('Add randomized UTMs?')
+    randomized_params = BooleanField('Add randomized URL params and values?')
+    submit_fakes = SubmitField()
