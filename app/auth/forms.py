017e48314a7361bdc988724f015bad5cb47e1368
IulianIV <ichim.iulian12@gmail.com>
2021-12-29 18:10:44 +0200

    Fixed some Typos. Added email field to Form.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,14 +1,12 @@
 from flask_wtf import FlaskForm
 
-from wtforms.fields import PasswordField, StringField, BooleanField, SubmitField
+from wtforms.fields import PasswordField, StringField, BooleanField, SubmitField, EmailField
 from wtforms.validators import DataRequired, EqualTo
 
 
 class LoginForm(FlaskForm):
-    # better-me consider the possibility to add Form names such as "Username" as a parameter
-    #   to the Form Field instantiation
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('remember me')
     submit = SubmitField()
 
@@ -17,4 +15,5 @@ class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     password_retype = PasswordField('Retype password', validators=[DataRequired(), EqualTo('password')])
+    email = EmailField('e-mail', validators=[DataRequired()])
     submit = SubmitField()
