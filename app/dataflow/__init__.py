7c48f531a272f3b1d79d2e2df4158dedf91aa43f
Iulian Ichim <ichim.iulian12@gmail.com>
2021-12-29 23:19:31 +0200

    Removed TODO

--- app/dataflow/__init__.py
+++ app/dataflow/__init__.py
@@ -1,5 +1,3 @@
 from flask import Blueprint
 
 bp = Blueprint('dataflow', __name__, url_prefix='/dataflow')
-
-# better-me Add Dataflow as part of the manager module which will contain several apps.
