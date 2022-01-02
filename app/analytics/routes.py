943e1968f0eb6b624192a20047ca89ff7374e4dd
Iulian Ichim <ichim.iulian12@gmail.com>
2022-01-02 17:33:43 +0200

    Small bug correction. Adapted logic to handle current_month queries.

--- app/analytics/routes.py
+++ app/analytics/routes.py
@@ -10,14 +10,13 @@ from matplotlib.figure import Figure
 from app.analytics import bp
 from app.manager.db.db_interrogations import *
 
-db_queries = Query()
 
 # TODO Add DataTables filtering to utm-stats table.
 
 
 @bp.route('/')
 def lifetime_expense():
-    expense_raw_data = db_queries.query_expense_entries()
+    expense_raw_data = query_expense_entries()
 
     expense_dict = {'expense_value': [expense.expense_value for expense in expense_raw_data]}
 
@@ -32,27 +31,34 @@ def lifetime_expense():
 # better-me maybe find a better name for this function and its template?
 @bp.route('/budget-tables')
 def budget_tables():
+
+    budget_queries = {
+        'expense_entries': query_expense_entries(),
+        'revenue_entries': query_revenue_entries(),
+        'savings_entries': query_savings_entries(),
+
+    }
     table_counts = {
-            'expense_count': db_queries.get_expense_count(),
-            'revenue_count': db_queries.get_revenue_count(),
-            'savings_count': db_queries.get_savings_count()
+            'expense_count': get_expense_count(),
+            'revenue_count': get_revenue_count(),
+            'savings_count': get_savings_count()
         }
 
     validation_counts = {
-        'validation_categories': db_queries.get_validation_categories_count(),
-        'validation_items': db_queries.get_validation_items_count(),
-        'validation_accounts': db_queries.get_validation_accounts_count(),
-        'validation_reason': db_queries.get_validation_reason_count(),
-        'validation_sources': db_queries.get_validation_sources_count()
+        'validation_categories': get_validation_categories_count(),
+        'validation_items': get_validation_items_count(),
+        'validation_accounts': get_validation_accounts_count(),
+        'validation_reason': get_validation_reason_count(),
+        'validation_sources': get_validation_sources_count()
     }
 
-    return render_template('analytics/budget_tables.html', db_queries=db_queries, table_counts=table_counts,
+    return render_template('analytics/budget_tables.html', budget_queries=budget_queries, table_counts=table_counts,
                            validation_counts=validation_counts)
 
 
 @bp.route('/utm-stats')
 def utm_stats():
-    raw_entries = db_queries.get_parsed_urls()
+    raw_entries = get_parsed_urls()
     num_of_params = []
     most_params = 0
     all_values_dict = {'date': [], 'url': [], 'qs': [], 'netloc': []}
