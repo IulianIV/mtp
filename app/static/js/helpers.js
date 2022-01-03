e72dae07f2a48dd87a4a0b547c4f715bec3c2748
Iulian Ichim <ichim.iulian12@gmail.com>
2022-01-04 01:21:50 +0200

    Move some files to be more organized. Working on getting data for the d3.js graphs from an API endpoint.

---
+++ app/static/js/helpers.js
@@ -0,0 +1,15 @@
+// Show/Hide EUR Preview jQuery
+// TODO add all jQuery related/AJAX/JS in here or relevant JS files
+$(document).ready(function(){
+  $('button[name="show_eurview"]').click(function(){
+    $('tr[class="table-primary eurview"]').show();
+    $('tr[class="table-primary eurview"]').show();
+    $('button[name="hide_eurview"]').show();
+  });
+  $('button[name="hide_eurview"]').click(function(){
+    $('tr[class="table-primary eurview"]').hide()
+    $('tr[class="table-primary eurview"]').hide()
+    $('button[name="hide_eurview"]').hide();
+
+  });
+});
