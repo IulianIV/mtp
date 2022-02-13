27c77ed4ebaa5c3aec2c0910aca15b8b3ae12ba0
Iulian Ichim <ichim.iulian12@gmail.com>
2022-02-14 00:09:25 +0200

    Created functionality that runs and configures Dropzone.js. Also includes functionality to send image at the API endpint and send UUID name at the function view level.

--- app/static/js/helpers.js
+++ app/static/js/helpers.js
@@ -15,3 +15,29 @@ $(document).ready(function () {
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+// DropzoneJS functionality config
+$("div#dropzone").dropzone({
+    url: '/api/post/image',
+    progressBarWidth: '100%',
+    maxFileSize: '5 MB',
+    uploadMultiple: false,
+    createImageThumbnails: true,
+    maxFiles: '1',
+    init: function () {
+        let dropzone = this;
+
+
+        // on button "close" or "X" press, remove dropzone instantiation.
+        this.on('sending', function(file, xhr, formData){
+            let acceptedFile = dropzone.getAcceptedFiles()[0];
+            formData.append('file_uuid', acceptedFile.upload.uuid);
+        });
+
+        $('input[id="submit_post"]').click(function () {
+            let acceptedFile = dropzone.getAcceptedFiles()[0];
+            document.getElementById("image_uuid").value = acceptedFile.upload.uuid;
+        });
+    }
+});
+
