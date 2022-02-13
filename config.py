ee2412c85cef448a3c4e37f7ac0dee3e9ef0771c
Iulian Ichim <ichim.iulian12@gmail.com>
2022-02-14 00:06:30 +0200

    Added config constant that points to uploads folder.

--- config.py
+++ config.py
@@ -8,10 +8,12 @@ class Config:
     FLASK_APP = 'mtp'
     FLASK_ENV = 'development'
     DEBUG = True
-
+    
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:admin@localhost/mtp'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG_TB_PROFILER_ENABLED = True
     DEBUG_TB_INTERCEPT_REDIRECTS = False
+    POST_IMAGE_UPLOAD_PATH = os.path.join(basedir, 'app\\uploads\\post\\')
+
 
 
