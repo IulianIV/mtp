8f44c9f58407c5421960aa9a2802cfe7e560760f
Iulian Ichim <ichim.iulian12@gmail.com>
2022-02-14 23:56:13 +0200

    Removed required validation on UUID Hidden Field. Added Hidden Field for Update function.

--- app/blog/forms.py
+++ app/blog/forms.py
@@ -10,11 +10,12 @@ from wtforms.validators import DataRequired
 class AddPost(FlaskForm):
     post_title = StringField('Title', validators=[DataRequired()])
     post_body = TextAreaField('Body', validators=[DataRequired()])
-    image_uuid = HiddenField('Image UUID', validators=[DataRequired()])
+    image_uuid = HiddenField('Image UUID')
     submit_post = SubmitField()
 
 
 class UpdatePost(FlaskForm):
     update_title = StringField('Update title', validators=[DataRequired()])
     update_body = TextAreaField('Update body', validators=[DataRequired()])
+    image_uuid = HiddenField('Image UUID')
     submit_update = SubmitField()
