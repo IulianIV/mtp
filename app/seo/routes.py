f65832a0edb10674524b72d9558a7c158d599f3b
Iulian Ichim <ichim.iulian12@gmail.com>
2022-02-15 23:57:25 +0200

    Added routing and some test functions for the gtm spy module.

--- app/seo/routes.py
+++ app/seo/routes.py
@@ -1,8 +1,38 @@
 from flask import request, send_from_directory, render_template
 
-from app import current_app
+from app import current_app, Config
 from app.auth.routes import login_required
+from app.manager.helpers import download_gtm_container_from_url, get_container_url, check_for_script
 from app.seo import bp
+from app.seo.gtmspy import GTMSpy
+
+model_gtm_id = 'GTM-T7MRFWX'
+
+
+@bp.route('/gtm-spy', methods=('GET', 'POST'))
+def gtm_spy():
+
+    if check_for_script(model_gtm_id):
+        print('GTM Script for given ID already exists.')
+    else:
+        download_gtm_container_from_url(Config.GTM_SPY_UPLOAD_PATH, model_gtm_id)
+
+    test = GTMSpy(model_gtm_id)
+    print(test)
+
+    for function in test.macros():
+        print(function)
+
+    for tag in test.tags():
+        print(tag)
+
+    for predicate in test.predicates():
+        print(predicate)
+
+    for rule in test.rules():
+        print(rule)
+
+    return render_template('seo/gtm_spy.html', model_gtm_path=get_container_url(model_gtm_id))
 
 
 @bp.route('/robots.txt')
