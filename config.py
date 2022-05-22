59fbd2d122944a109669d7a17ee2bacce833bf81
Iulian Ichim <ichim.iulian12@gmail.com>
2022-05-22 21:07:51 +0300

    added deployment config info

--- config.py
+++ config.py
@@ -16,5 +16,22 @@ class Config:
     POST_IMAGE_UPLOAD_PATH = os.path.join(basedir, 'app\\uploads\\post\\')
     GTM_SPY_DOWNLOAD_PATH = os.path.join(basedir, 'app\\uploads\\gtm_scripts')
 
+    '''
+    PythonAnywhere Database Config
+    
+    They are to be added extra or in place of what is above.
+    
+    db_u = 'user'
+    db_p = 'pass'
+    db_h = 'maiels.mysql.pythonanywhere-services.com'
+    db_d = 'mtpdb'
+    
+    SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{db_u}:{db_p}@{db_h}/{db_d}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_RECYCLE = 299
+
+    
+    '''
+
 
 
