80807fdb0d67308af5ffe10094bce899f957e313
IulianIV <ichim.iulian12@gmail.com>
2022-06-03 13:03:02 +0000

    Removed 'linkchecker' from TinyMC. Styling fixes. Fixed DEBUG conditional styling choice.

--- app/static/js/wysiwyg_editor.js
+++ app/static/js/wysiwyg_editor.js
@@ -1,7 +1,7 @@
 tinymce.init({
     selector: 'textarea',
     skin: 'bootstrap',
-    plugins: 'lists link linkchecker',
+    plugins: 'lists link',
     toolbar: 'h1 h2 bold italic strikethrough blockquote bullist numlist backcolor forecolor | link | removeformat help',
     menubar: false
 });
