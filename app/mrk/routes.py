29666e991989594c56c0c51cdf19f10bbc0fa70e
Iulian Ichim <ichim.iulian12@gmail.com>
2022-07-02 23:15:09 +0300

    Testing new functionalities.

--- app/mrk/routes.py
+++ app/mrk/routes.py
@@ -8,10 +8,18 @@ model_gtm_id = 'GTM-T7MRFWX'
 
 @bp.route('/gtm-spy', methods=('GET', 'POST'))
 def gtm_spy():
-
     spy = Container(model_gtm_id)
     container_url = spy.url
-    predicate = spy.process_predicate(5, detailed=True)
-    print(predicate)
+    spy.process_rules()
+
+    example_map = ["list",
+                   ["map", "keyz", "lookup_input", "value", "lookup_output"],
+                   ["map", "keyd", "lookup_input", "value", ["macro", 5]],
+                   ["map", "keyr", ["macro", 9], "value", "lookup_output"]
+                   ]
+
+    spy.process_type()
+
+    print(spy.process_mapping(example_map))
 
     return render_template('mrk/gtm_spy/index.html', model_gtm_path=container_url)
