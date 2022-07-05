16e24ff076618256c6f7806d11d82aa67c54e9b1
Iulian Ichim <ichim.iulian12@gmail.com>
2022-07-05 23:59:59 +0300

    Created two more routes to test some designs

--- app/mrk/routes.py
+++ app/mrk/routes.py
@@ -1,25 +1,50 @@
 from flask import render_template
 
 from app.mrk import bp
-from app.mrk.gtm_spy.container import Container
+from app.mrk.gtm_spy.gtmintel import GTMIntel
+from app.mrk.gtm_spy.index import macros_index, tags_index
+from app.mrk.gtm_spy.lurker import find_in_index
 
 model_gtm_id = 'GTM-T7MRFWX'
 
+spy = GTMIntel(model_gtm_id)
+container_url = spy.url
+container_id = model_gtm_id
+container_version = spy.version
+
+# TODO should the final container contain data for color coding?
+# TODO add modal preview for lists and certtain variables
+
 
 @bp.route('/gtm-spy', methods=('GET', 'POST'))
-def gtm_spy():
-    spy = Container(model_gtm_id)
-    container_url = spy.url
-    spy.process_rules()
+def gtm_intel():
+    return render_template('mrk/gtm_spy/index.html', model_gtm_path=container_url,
+                           gtm_id=container_id, version=container_version)
 
-    example_map = ["list",
-                   ["map", "keyz", "lookup_input", "value", "lookup_output"],
-                   ["map", "keyd", "lookup_input", "value", ["macro", 5]],
-                   ["map", "keyr", ["macro", 9], "value", "lookup_output"]
-                   ]
 
-    spy.process_type()
+# fixme this error passes silently. Deal with KeyError cases.
+@bp.route('/gtm-spy/tags', methods=('GET', 'POST'))
+def gtm_intel_tags():
+    tags = spy.tags
+    tag_list = []
 
-    print(spy.process_mapping(example_map))
+    for tag in tags:
+        try:
+            tag_list.append(find_in_index(tag['function'], tags_index))
+        except KeyError:
+            continue
 
-    return render_template('mrk/gtm_spy/index.html', model_gtm_path=container_url)
+    return render_template('mrk/gtm_spy/tags.html', model_gtm_path=container_url,
+                           gtm_id=container_id, version=container_version, tag_list=tag_list)
+
+
+@bp.route('/gtm-spy/variables', methods=('GET', 'POST'))
+def gtm_intel_variables():
+    macros = spy.macros
+    variables = []
+
+    for var in macros:
+        variables.append(find_in_index(var['function'], macros_index))
+
+    return render_template('mrk/gtm_spy/variables.html', model_gtm_path=container_url,
+                           gtm_id=container_id, version=container_version, variables=variables)
