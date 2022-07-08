3ab91d7688be18bf9897c5137c82109cb0c08e2d
Iulian Ichim <ichim.iulian12@gmail.com>
2022-07-08 16:37:10 +0300

    Finished variable presentation in the front-end. Not much done back-end wise. Everything was done in jinja2.

--- app/mrk/routes.py
+++ app/mrk/routes.py
@@ -2,7 +2,7 @@ from flask import render_template
 
 from app.mrk import bp
 from app.mrk.gtm_spy.gtmintel import GTMIntel
-from app.mrk.gtm_spy.index import macros_index, tags_index
+from app.mrk.gtm_spy.index import tags_index, skip_macro_keys, macros_index
 from app.mrk.gtm_spy.lurker import find_in_index
 
 model_gtm_id = 'GTM-T7MRFWX'
@@ -18,6 +18,9 @@ container_version = spy.version
 
 @bp.route('/gtm-spy', methods=('GET', 'POST'))
 def gtm_intel():
+
+
+
     return render_template('mrk/gtm_spy/index.html', model_gtm_path=container_url,
                            gtm_id=container_id, version=container_version)
 
@@ -40,11 +43,22 @@ def gtm_intel_tags():
 
 @bp.route('/gtm-spy/variables', methods=('GET', 'POST'))
 def gtm_intel_variables():
-    macros = spy.macros
-    variables = []
 
-    for var in macros:
-        variables.append(find_in_index(var['function'], macros_index))
+    variables = spy.create_macro_container()
+    skip_keys = skip_macro_keys
 
+    type_check = spy.process_type
+    get_macro = spy.process_macro
+    find_index = find_in_index
+    macro_index = macros_index
+    process_mapping = spy.process_mapping
+
+    # Creates a new dictionary that contains:
+    #   1. All the relevant data from the original container
+    #   2. Dict keys with all the relevant information from the index
+    #   3. When parsing the final dict in the front-end these dict keys should not be shown in the modal
+    #       title, isBuiltin, pill, nameProperty, function
     return render_template('mrk/gtm_spy/variables.html', model_gtm_path=container_url,
-                           gtm_id=container_id, version=container_version, variables=variables)
+                           gtm_id=container_id, version=container_version, variables=variables,
+                           skip_macro_keys=skip_keys, type_check=type_check, get_macro=get_macro,
+                           macros_index=macro_index, find_index=find_index, process_mapping=process_mapping)
