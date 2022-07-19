3085d63969de307d1c0e3c4344390b0255395051
Iulian Ichim <ichim.iulian12@gmail.com>
2022-07-19 23:53:49 +0300

    Added database model for GTM container.

--- app/manager/db/models.py
+++ app/manager/db/models.py
@@ -283,3 +283,15 @@ class UrlEncodeDecodeParse(db.Model):
 
     def __repr__(self):
         return f'URL with id {self.id} is: {self.raw_url}'
+
+
+class GTMContainers(db.Model):
+    __tablename__ = 'gtm_containers'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(user_id_fk))
+    container_id = db.Column(db.String(25), index=True, nullable=False)
+    container_data = db.Column(db.BLOB, index=False, nullable=False)
+    is_active = db.Column(db.Boolean, index=False, nullable=False, default=False)
+
+    def __repr__(self):
+        return f'GTM Container of id {self.container_id}.'
