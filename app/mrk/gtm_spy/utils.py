c8bde82e334869312181d0833d46e6c552381968
Iulian Ichim <ichim.iulian12@gmail.com>
2022-07-22 22:44:01 +0300

    Moved around some files and refactored some functions.

--- app/mrk/gtm_spy/utils.py
+++ app/mrk/gtm_spy/utils.py
@@ -15,3 +15,22 @@ def gtm_compare_get_version(gtm_id) -> int:
     container_json = json.loads(container_data)
 
     return container_json['resource']['version']
+
+
+# better-me add fallback situations when there are no arguments given.
+def find_in_index(value: str, index: dict) -> dict:
+    """
+    Attempts to find 'value' in 'index'.
+    Considering that a container hold a lot of information, it is best to index items in dictionaries that
+    hold other dictionaries as values, even in the case of single key-value pairs.
+    Keeping this consistency avoids having to write exception cases.
+
+    :param value: Dictionary key to look for
+    :type value: string
+    :param index: Dictionary to search into
+    :type index: dictionary
+    :return: Dictionary containing value data
+    :rtype: dictionary
+    """
+
+    return index[value]
