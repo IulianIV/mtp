3c3f0a820b61638b921b3db17326fbbec36435bf
Iulian Ichim <ichim.iulian12@gmail.com>
2022-09-07 00:14:14 +0300

    Quick and easy implementation of the url_map to module_map function

--- app/manager/permissions/routes.py
+++ app/manager/permissions/routes.py
@@ -1,9 +1,15 @@
+from collections import defaultdict
+from typing import Iterator
+
 from flask import (
     redirect, render_template, request, url_for, current_app
 )
 from flask_login import current_user
-from app.manager.helpers import login_required
+from app.manager.helpers import login_required, extract_nested_strings
 from app.manager.permissions import bp
+from app.manager.permissions.utils import convert_url_map_to_module_map
+
+MODULE_EXCEPTIONS = ['debugtoolbar', 'api', 'auth', '_debug_toolbar', 'static', 'static_from_root']
 
 
 @bp.route('/user-roles/view', methods=('GET',))
@@ -14,12 +20,16 @@ def user_roles_view():
     return render_template('manager/permissions/view_user_roles.html')
 
 
+# TODO Must have in mind the following: assuming that we have a collection of modules and submodules that upon user
+#   selection become a JSON object that is stored in a database, what happens when and if an endpoint is modified
+#   or even removed?
+#   If an endpoint is removed it is pretty easy, I suppose - you reparse the rules, create the JSON and update the DB
+#   but eliminate the differences between them.
+#   What if an endpoint is just plain modified?
 @bp.route('/user-roles/create', methods=('POST', 'GET'))
 def user_roles_create():
+    rule_map = current_app.url_map.iter_rules()
 
-    all_urls = current_app.url_map.iter_rules()
-    for url in all_urls:
-        print(f'URL endpoint: {url.endpoint}')
-        print(f'URL rule: {url.rule}')
+    module_map = convert_url_map_to_module_map(rule_map, skip_list=MODULE_EXCEPTIONS)
 
-    return render_template('manager/permissions/create_user_roles.html')
+    return render_template('manager/permissions/create_user_roles.html', module_map=module_map)
