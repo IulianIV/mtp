90ef61eede6084a05bc4bd69515f7956862ac499
Iulian Ichim <ichim.iulian12@gmail.com>
2022-09-09 07:54:18 +0300

    Added All Users view. Working on properly handling new user role creation.

--- app/manager/permissions/routes.py
+++ app/manager/permissions/routes.py
@@ -1,14 +1,14 @@
 from flask import (
-    redirect, render_template, url_for, current_app
+    render_template, current_app, request, redirect, url_for
 )
-from flask_login import current_user
-from app.manager.permissions.utils import login_required, requires_permissions
+from app.manager.permissions.utils import login_required, requires_permissions,\
+    convert_url_map_to_module_map, process_rule_list
 from app.manager.permissions import bp
-from app.manager.permissions.utils import convert_url_map_to_module_map
-from app.manager.db.db_interrogations import get_user_role_rules
+from app.manager.db.db_interrogations import get_all_roles, get_all_users
+from app.manager.helpers import form_validated_message, form_error_message
+from app import Config
 
-MODULE_EXCEPTIONS = ['debugtoolbar', 'api', 'auth', '_debug_toolbar', 'static', 'static_from_root']
-
+from app.manager.permissions import forms
 
 # TODO Must have in mind the following: assuming that we have a collection of modules and submodules that upon user
 #   selection become a JSON object that is stored in a database, what happens when and if an endpoint is modified
@@ -16,18 +16,54 @@ MODULE_EXCEPTIONS = ['debugtoolbar', 'api', 'auth', '_debug_toolbar', 'static', 
 #   If an endpoint is removed it is pretty easy, I suppose - you reparse the rules, create the JSON and update the DB
 #   but eliminate the differences between them.
 #   What if an endpoint is just plain modified?
-@bp.route('/user-roles', methods=('GET',))
+
+
+@bp.route('/user-roles', methods=('GET', 'POST'))
 @requires_permissions
 @login_required
 def user_roles():
+    rules_form = forms.RulesForm()
 
+    all_roles = get_all_roles()
     rule_map = current_app.url_map.iter_rules()
 
-    module_map = convert_url_map_to_module_map(rule_map, skip_list=MODULE_EXCEPTIONS)
+    module_map = convert_url_map_to_module_map(rule_map, skip_list=Config.MODULE_EXCEPTIONS)
 
-    print(module_map)
+    # HTML forms limitations enforce usage of requests to get checkbox array data
+    # read more info in permissions/forms.py comments
+    if request.method == 'POST':
+        if rules_form.is_submitted() and rules_form.validate_on_submit():
+            selected_modules_list = request.form.getlist('module_name')
 
-    return render_template('manager/permissions/user_roles.html', module_map=module_map)
+            print(selected_modules_list)
 
 
+            rule_list = process_rule_list(selected_modules_list, module_map)
 
+
+            if selected_modules_list == ['']:
+                form_error_message('You can not create a Role with no rules.')
+
+                return redirect(url_for('permissions.user_roles'))
+
+            form_validated_message(f'New role "{rules_form.role_name.data}" '
+                                   f'has been submitted. It will become available in a few moments')
+
+            return redirect(url_for('permissions.user_roles'))
+
+            # add function that processes the selected list.
+            # if in the selected list is a Parent module name remove parent, remove children and
+            # re-populate with parents children (basically give module wide access)
+
+    return render_template('manager/permissions/user_roles.html', rules_form=rules_form,
+                           module_map=module_map, all_roles=all_roles)
+
+
+@bp.route('/all-users', methods=('GET', 'POST'))
+@requires_permissions
+@login_required
+def all_users():
+
+    users = get_all_users()
+
+    return render_template('manager/permissions/all_users.html', all_users=users)
