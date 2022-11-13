1fd562125b5ab676f1f0b8bb62f97435dafd50a4
Iulian Ichim <ichim.iulian12@gmail.com>
2022-11-13 23:56:12 +0200

    Updated indexes

--- app/mrk/tag_spy/index.py
+++ app/mrk/tag_spy/index.py
@@ -97,12 +97,12 @@ class RuntimeArray(Enum):
     key_value_based = '{}'
 
 
-skippable_macro_keys = ['title', 'isBuiltin', 'pill', 'nameProperty', 'function', 'infoKey']
+skippable_macro_keys = ['title', 'isBuiltin', 'pill', 'nameProperty', 'function', 'infoKey', 'macro_idx']
 skippable_tag_keys = ['pill', 'property', 'nameProperty', 'title', 'function', 'infoKey', 'teardown_tags',
-                      'setup_tags', '_sequence', '_conditions', '_blocking', 'exportType']
+                      'setup_tags', '_sequence', '_conditions', '_blocking', 'exportType', 'tag_idx', 'macro_idx']
 skippable_trigger_keys = ['gtm.triggerGroup', '__tg']
 triggers_not_tags = ['__tl', '__tg', '__cl', '__lcl', '__evl', '__sdl']
-code_snippet_properties = ['vtp_html', 'vtp_javascript']
+code_snippet_properties = ['vtp_html', 'vtp_javascript', '__jsm']
 untracked_macros = ['Environment name']
 runtime_function_regex = [r'__cvt_\d+_\d+', r'__awec', r'__baut', r'__crto', r'__pntr']
 macro_data_keys = ['vtp_name', 'vtp_javascript', 'vtp_html', 'vtp_url', 'vtp_trackingId',
